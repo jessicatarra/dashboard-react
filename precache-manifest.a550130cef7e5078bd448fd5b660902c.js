9aaebb242cdc701a89ce8eeb89bced7f9124623e
Jessica <jessictarra@gmail.com>
2020-07-15 17:26:21 -0300

    Updates

---
+++ precache-manifest.a550130cef7e5078bd448fd5b660902c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5acdc20570deacd877dbb1b2f0cf4847",
+    "url": "/dashboard-react/index.html"
+  },
+  {
+    "revision": "6b093eb26c0fdc9dac24",
+    "url": "/dashboard-react/static/css/main.9ec81365.chunk.css"
+  },
+  {
+    "revision": "fb870bb248d2b91f77f5",
+    "url": "/dashboard-react/static/js/2.3246359d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dashboard-react/static/js/2.3246359d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b093eb26c0fdc9dac24",
+    "url": "/dashboard-react/static/js/main.9d915093.chunk.js"
+  },
+  {
+    "revision": "2ff7211a729aa1ac1ad9",
+    "url": "/dashboard-react/static/js/runtime-main.6876af23.js"
+  }
+]);
